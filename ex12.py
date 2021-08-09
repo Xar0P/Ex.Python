389fbe36d8590566f5c9bb34df0e110c300a9b4d
Xar0P <pietroricardocres@hotmail.com>
2021-08-09 09:21:26 -0300

    updated

--- ex12.py
+++ ex12.py
@@ -14,7 +14,7 @@ def main():
     area = area(largura, altura)
     tinta = tinta(area) 
 
-    print(f'O valor da área dessa parede é {area}m²\nA quantidade de litros de tinta para pintar essa parede é {tinta}{"l" if tinta > 1 else "ml"}')
+    print(f'O valor da área dessa parede é {area}m²\nA quantidade de litros de tinta para pintar essa parede é {tinta}{"l" if tinta >= 1 else "ml"}')
 
 if __name__ == '__main__':
     main()
