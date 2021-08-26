9bc54e354ef2b6ad1aa0d2c736b4615d9150ccca
Xar0P <pietroricardocres@hotmail.com>
2021-08-25 21:31:44 -0300

    bug fix

--- 24_08_2021/ex18.py
+++ 24_08_2021/ex18.py
@@ -4,7 +4,7 @@
 # seguido de uma descrição da despesa, e da data que o item foi criado.
 
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from datetime import date
 from PIL import ImageTk, Image # pip install Pillow
 import csv
@@ -39,16 +39,31 @@ import csv
 #     def add_funcionario(self, funcionario: object) -> None:
 #         self.funcionarios.append(funcionario)
 
+class NoBank(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+
+class InvalidField(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+
 class Bancos:
 
     def procurar(self, cod: str = None) -> str:
+        if cod == '':
+            raise InvalidField()
+
         with open('24_08_2021/Bancos Associados.csv','r', encoding='UTF-8') as file:
             for line in file:
                 line = line.split(';')
                 if line[0] == cod:
                     return line
         
-        raise Exception('Banco não encontrado!')
+        raise NoBank()
 
     def tratar_cod(self, cod_banco) -> str:
         if len(cod_banco) == 1:
@@ -62,22 +77,33 @@ class Bancos:
         destino.valor_banco += valor
 
 
+
 class Contrato(Bancos):
 
-    def __init__(self, cod_banco: str = None, val_despesa: str = '1100,1', desc_despesa: str = None) -> None:
+    def __init__(self, cod_banco: str = None, val_despesa: str = None, desc_despesa: str = None) -> None:
         self.cod_banco = self.tratar_cod(cod_banco)
         self.info_banco = self.procurar(self.cod_banco)
 
         self.val_despesa = self._despesa_float(val_despesa)
-        self.desc_despesa = desc_despesa
+        self.desc_despesa = self._tratar_despesa(desc_despesa)
         self.data_registro = date.today().strftime('%d/%m/%Y')
 
     def __str__(self) -> str:
         return f'\nBanco: {self.info_banco[0]} - {self.info_banco[1]}\n\nValor: R${self.val_despesa:,.2f}\nDescrição: {self.desc_despesa}\nData do registro: {self.data_registro}\n'
 
     def _despesa_float(self, despesa) -> float:
+        if despesa == '':
+            raise InvalidField()
+
         despesa = despesa.replace('.','').replace(',','.')
         return float(despesa)
+
+
+    def _tratar_despesa(self, desc) -> str:
+        if desc == '':
+            raise InvalidField()
+
+        return desc
 
 
 
@@ -114,9 +140,15 @@ class App(tk.Frame):
 
 
     def _config_window(self, dir_icon: str, title: str, size_w: str, size_h: str) -> None:
-        self.master.tk.call('wm','iconphoto', self.master._w, ImageTk.PhotoImage(Image.open(dir_icon)))
+        
+        try:
+            self.master.tk.call('wm','iconphoto', self.master._w, ImageTk.PhotoImage(Image.open(dir_icon)))
+        except Exception:
+            pass
+
         self.master.title(title)
         self.master.minsize(size_w,size_h)
+        self.master.resizable(0,0)
 
     def _criar_widgets(self) -> None:
 
@@ -126,8 +158,8 @@ class App(tk.Frame):
         button.place(relwidth=0.90,relheight=0.08, rely=0.4, relx=0.05)
         self.master.bind('<Return>',lambda event=None: button.invoke())
 
-        self.frame_results = ttk.Labelframe(self.master,text='Resultado')
-        self.frame_results.place(relwidth=1, relheight=0.5, rely=0.5)
+        self.frame_results = ttk.Labelframe(self.master)
+        self.frame_results.place(relwidth=1, relheight=0.5, rely=0.55)
 
     def _entry_section(self) -> None:
 
@@ -160,22 +192,34 @@ class App(tk.Frame):
 
     def _get_inputs(self) -> None:
 
-        if self.inside:
-            self.inside.destroy()
+        try:
+            if self.inside:
+                self.inside.destroy()
 
-        value_cod = self.get_input(self.input_cod)
-        value_despesa = self.get_input(self.input_val_desp)
-        value_desc = self.get_input(self.input_desc)
+            value_cod = self.get_input(self.input_cod)
+            value_despesa = self.get_input(self.input_val_desp)
+            value_desc = self.get_input(self.input_desc)
 
-        contrato = Contrato(value_cod, value_despesa, value_desc)
-        self.contratos.add_contrato(contrato)
 
-        self.inside = tk.Label(self.frame_results, text=contrato)
-        self.inside.pack()
+            contrato = Contrato(value_cod, value_despesa, value_desc)
+            self.contratos.add_contrato(contrato)
 
-        self.input_cod.delete(0,'end')
-        self.input_val_desp.delete(0,'end')
-        self.input_desc.delete(0,'end')
+            self.inside = tk.Label(self.frame_results, text=contrato)
+            self.inside.pack()
+
+            self.input_cod.delete(0,'end')
+            self.input_val_desp.delete(0,'end')
+            self.input_desc.delete(0,'end')
+
+        except InvalidField:
+            messagebox.showerror(title='Erro',message='Insira todos os campos!')
+        except NoBank:
+            messagebox.showerror(title='Erro',message='Banco não encontrado!')
+        except ValueError:
+            messagebox.showerror(title='Erro',message='Valor da despesa inválido!')
+        except Exception as e:
+            messagebox.showerror(title='Erro',message='Ocorreu um erro! Tente novamente.')
+
 
 
     def get_input(self, input_name):
@@ -201,7 +245,7 @@ if __name__ == '__main__':
                 'ID':i,
                 'BANCO':f'{contrato.info_banco[0]} - {contrato.info_banco[1]}',
                 'VALOR':f'R${contrato.val_despesa:,.2f}',
-                'descrição':contrato.desc_despesa,
+                'DESCRIÇÃO':contrato.desc_despesa,
                 'REGISTRO':contrato.data_registro
             })
             i += 1
